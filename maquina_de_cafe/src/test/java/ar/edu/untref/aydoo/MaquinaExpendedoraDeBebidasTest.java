5c208c55c7e5d7ab0ae748e9f21db15864972cc8
ferGainey <fernandogainey@hotmail.com>
2016-03-29 00:24:08 -0300

    se agrega N cantidad positiva de azucar

--- maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidasTest.java
+++ maquina_de_cafe/src/test/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidasTest.java
@@ -23,4 +23,20 @@ public class MaquinaExpendedoraDeBebidasTest {
 		Assert.assertEquals(1, miVaso.getContenido().size());
 		Assert.assertEquals("leche", miVaso.getContenido().get(0));
 	}
+	
+	@Test
+	public void ponerNAzucar(){
+		Vaso miVaso1 = new Vaso();
+		Vaso miVaso2 = new Vaso();
+		Azucarero miAzucarero1 = new Azucarero();
+		miAzucarero1.ponerNEnEsteVaso(miVaso1, 1);
+		Assert.assertEquals(1, miVaso1.getContenido().size());
+		Assert.assertEquals("azucar", miVaso1.getContenido().get(0));
+		Azucarero miAzucarero2 = new Azucarero();
+		miAzucarero2.ponerNEnEsteVaso(miVaso2, 3);
+		Assert.assertEquals(3, miVaso2.getContenido().size());
+		Assert.assertEquals("azucar", miVaso2.getContenido().get(0));
+		Assert.assertEquals("azucar", miVaso2.getContenido().get(1));
+		Assert.assertEquals("azucar", miVaso2.getContenido().get(2));
+	}
 }
