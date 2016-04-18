7bf7e666970feb63307f2bf271e54d35e1dc5b99
ferGainey <fernandogainey@hotmail.com>
2016-04-18 01:48:05 -0300

    la logica de formato interpreta el formato pretty

--- factores-primos-3/src/test/java/ar/edu/untref/aydoo/LogicaDeFormatoTest.java
+++ factores-primos-3/src/test/java/ar/edu/untref/aydoo/LogicaDeFormatoTest.java
@@ -7,51 +7,27 @@ import org.junit.Assert;
 public class LogicaDeFormatoTest {
 
 	@Test
-	public void interpretarFormatoPretty(){
+	public void interpretarFormatoQuiet(){
 		LogicaDeFormato miLogica = new LogicaDeFormato("--format=quiet");
 		miLogica.interpretarFormato();
 		Assert.assertEquals("quiet", miLogica.getTipoDeFormato());
 	}
+	
+	@Test
+	public void interpretarFormatoQuietConMayusculasYMinusculas(){
+		LogicaDeFormato miLogica = new LogicaDeFormato("--format=QuIEt");
+		miLogica.interpretarFormato();
+		Assert.assertEquals("quiet", miLogica.getTipoDeFormato());
+	}
+	
+	@Test
+	public void interpretarFormatoPretty(){
+		LogicaDeFormato miLogica = new LogicaDeFormato("--format=pretty");
+		miLogica.interpretarFormato();
+		Assert.assertEquals("pretty", miLogica.getTipoDeFormato());
+	}
 	/*
-	@Test
-	public void invertirListaDeVariosElementos(){
-		Program program = new Program();
-		ArrayList<Integer> listaNoOrdenada = new ArrayList<Integer>();
-		listaNoOrdenada.add(0, 1);listaNoOrdenada.add(1, 2);listaNoOrdenada.add(2, 3);
-		listaNoOrdenada.add(3, 4);listaNoOrdenada.add(4, 5);
-		ArrayList<Integer> esperado = new ArrayList<Integer>();
-		esperado.add(0, 5);esperado.add(1, 4);esperado.add(2, 3);esperado.add(3, 2);
-		esperado.add(4, 1);
-		ArrayList<Integer> listaObtenida = program.invertirLista(listaNoOrdenada);
-		Assert.assertEquals(esperado, listaObtenida);  	
-	}
 
-	@Test
-	public void invertirListaDeUnElemento(){
-		Program program = new Program();
-		ArrayList<Integer> listaNoOrdenada = new ArrayList<Integer>();
-		listaNoOrdenada.add(0, 1);
-		ArrayList<Integer> esperado = new ArrayList<Integer>();
-		esperado.add(0, 1);
-		ArrayList<Integer> listaObtenida = program.invertirLista(listaNoOrdenada);
-		Assert.assertEquals(esperado, listaObtenida);  	
-	}
-
-	@Test
-	public void leerFormatoPretty(){
-		Program program = new Program();
-		program.setFormato("--format=pretty");
-		program.leer();
-		Assert.assertEquals(1, program.getTipoDeFormato());
-	}
-
-	@Test
-	public void leerFormatoPrettyConMayusculasYMinusculas(){
-		Program program = new Program();
-		program.setFormato("--format=PRetTY");
-		program.leer();
-		Assert.assertEquals(1, program.getTipoDeFormato());
-	}
 
 	// como no pone nunca que va a querer escribir en algun formato (con la palabra --format)
 	// entonces corre por defecto el formato pretty
@@ -63,14 +39,7 @@ public class LogicaDeFormatoTest {
 		Assert.assertEquals(1, program.getTipoDeFormato());
 	}
 
-	@Test
-	public void leerFormatoQuiet(){
-		Program program = new Program();
-		program.setFormato("--format=quiet");
-		program.leer();
-		Assert.assertEquals(2, program.getTipoDeFormato());
-	}
-
+	
 	@Test
 	public void pruebaSepararPalabra(){
 		Program program = new Program();
@@ -81,15 +50,6 @@ public class LogicaDeFormatoTest {
 		Assert.assertEquals("--format", palabra1);
 		Assert.assertEquals("quiet", palabra2);
 		Assert.assertEquals(2, palabraSeparada.length);
-	}
-
-
-	@Test
-	public void leerFormatoQuietConMayusculasYMinusculas(){
-		Program program = new Program();
-		program.setFormato("--format=qUIeT");
-		program.leer();
-		Assert.assertEquals(2, program.getTipoDeFormato());
 	}
 
 
